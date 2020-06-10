5d0dc87f417083eb9a9a7c3df84745571243a550
dg-dhg <630909931@qq.com>
2020-06-10 20:20:29 +0800

    叶子拓展春天安全，实现授权认证，记住我功能

---
+++ src/main/java/net/dhg/jdbc/config/NewASpringSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package net.dhg.jdbc.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//开启web 安全模式，被security托管
+@EnableWebSecurity
+public class NewASpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /*这个方法被重写很多次，看来是个比较重要的功能，链式编程*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //1拦截请求（匹配身份和请求地址）授权？
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/loginUser/level1/**").hasRole("vip1")
+                .antMatchers("/loginUser/level2/**").hasRole("vip2")
+                .antMatchers("/loginUser/level3/**").hasRole("vip3");
+        //2登录页
+        http.formLogin().loginPage("/loginUser./login");
+
+        //3.对注销进行一些操作
+        //.logout().deleteCookies(&quot;remove&quot;).invalidateHttpSession(false)//一般不清空session和cookie
+        //.logoutUrl(&quot;/custom-logout&quot;).logoutSuccessUrl(&quot;/logout-success&quot;);
+        http.logout().logoutSuccessUrl("/loginUser/").permitAll();
+
+        //4.关闭跨站请求伪造攻击防御,登录登出根据get请求进行交互,而这涉及到Session和cookie的操作,
+        //这在spring security 看来是不安全的
+        http.csrf().disable();
+
+        //5.勿忘我 默认最长记忆时间两周
+        http.rememberMe();
+
+    }
+
+    /*认证。。。在2.1.x时可用，在2.2.x后需加密密码
+    * 在ss5（SpringSecurity中有许多的加密的方法5）'
+    * 这里推荐使用BCryptEncoder
+    * */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*这是在内存中设置账号和密码，一般是在数据库中查找进行校验*/
+        BCryptPasswordEncoder encoder =new BCryptPasswordEncoder();
+        auth.inMemoryAuthentication().passwordEncoder(encoder)
+                .withUser("dhg").password(encoder.encode("123456")).roles("vip1", "vip2")
+                .and().withUser("root").password(encoder.encode("123456")).roles("vip1", "vip2", "vip3")
+                .and().withUser("guest").password(encoder.encode("111222")).roles("vip1");
+    }
+}
--- src/main/java/net/dhg/jdbc/controller/RouterController.java
+++ src/main/java/net/dhg/jdbc/controller/RouterController.java
@@ -1,30 +1,37 @@
 package net.dhg.jdbc.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/loginUser")
+@Slf4j
 public class RouterController {
     @RequestMapping({"/login"})
     public ModelAndView login(HttpServletResponse response, HttpServletRequest request) throws Exception {
         return new ModelAndView("login");
     }
 
-    @RequestMapping("/index")
-    public ModelAndView index(HttpServletResponse response) throws Exception {
+    @RequestMapping({"/index", "/"})
+    public ModelAndView index(HttpServletResponse response,HttpServletRequest request) throws Exception {
+
         return new ModelAndView("index");
     }
 
     @RequestMapping("/level{level}/{id}")
-    public  ModelAndView  toLevel(@PathVariable("level") String level,@PathVariable("id") String id ,HttpServletResponse response) throws Exception{
-        String url=String.format("level%s/%s",level,id);
-        System.out.println("debug====>"+url);
+    public ModelAndView toLevel(@PathVariable("level") String level, @PathVariable("id") String id, HttpServletRequest request) {
+        String url = String.format("level%s/%s", level, id);
+        System.out.println("debug====>" + url);
         return new ModelAndView(url);
     }
 }
